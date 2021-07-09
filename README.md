898d856f714af3471c3e5ccc75cca48af9503807
Naveigell <yosua.yerikho123@gmail.com>
2021-07-09 13:07:00 +0800

    Add custom messages and store to session feature

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ You can add data you want
 require_once "Validator.php";
 
 $validator = new Validator();
+$validator->storeToSession(true);
 // make validation rules
 $validator->make([
     "username"      => "admin1",
@@ -17,6 +18,11 @@ $validator->make([
 ], [
     "username"      => [
         "rules"     => "required|min:5|max:6",
+        // you can create custom messages into each rule like this
+        "messages"  => [
+            "required"   => "Username harus diisi",
+            "min"        => "Panjang karakter username minimal 5" 
+        ]          
     ],
     "password"      => [
         "rules"     => "required|min:9|max:19",
@@ -44,6 +50,9 @@ if (!$validator->success()) {
 require_once "Validator.php";
 
 $validator = new Validator();
+// set store to session to TRUE if you want to store the message
+// into session
+$validator->storeToSession(true);
 // make validation rules
 $validator->make([
     "username"      => "admin1",
@@ -67,6 +76,13 @@ $validator->make([
 
 if (!$validator->success()) {
     var_dump($validator->messages());
+
+    // if store to session set to TRUE, the message will store to session
+    // and the key name started with "error-*"
+    // example, if username and password error, you can get it with
+    var_dump($_SESSION["error-username"]);
+    var_dump($_SESSION["error-password"]);
+    
 } else {
     echo "nothing wrong";
 }
--- index.php
+++ index.php
@@ -1,29 +1,38 @@
 <?php
+session_start();
 require_once "Validator.php";
 
 $validator = new Validator();
+//$validator->storeToSession(true);
 $validator->make([
-    "username"      => "admin1",
-    "password"      => "password1234",
-    "number"        => "3",
-    "array"         => "male"
+    "username"                  => "ad1ffff",
+    "password"                  => "password1234",
+    "number"                    => "pd",
+    "array"                     => "ma1le"
 ], [
-    "username"      => [
-        "rules"     => "required|min:5|max:6",
+    "username"                  => [
+        "rules"                 => "required|min:5|max:6",
+        "messages"              => [
+            "required"          => "Username harus diisi",
+            "min"               => "Panjang karakter username minimal 5",
+            "max"               => "Panjang karakter username maksimal 6",
+        ]
     ],
-    "password"      => [
-        "rules"     => "required|min:9|max:19",
+    "password"                  => [
+        "rules"                 => "required|min:9|max:19",
     ],
-    "number"        => [
-        "rules"     => "required|integer|digits_between:1,5",
+    "number"                    => [
+        "rules"                 => "required|min:3",
     ],
-    "array"         => [
-        "rules"     => "required|in:male,female",
+    "array"                     => [
+        "rules"                 => "required|in:male,female",
     ]
 ]);
 
 if (!$validator->success()) {
+    echo "<pre>";
     var_dump($validator->messages());
+    echo "</pre>";
 } else {
     echo "nothing wrong";
 }
